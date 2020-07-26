e5b5d287219d2d510f8896f8fef570adff216473
Murilo <43999445+murilobellatini@users.noreply.github.com>
2020-07-26 10:51:14 -0300

    Integrate Jinja templates

--- app/views.py
+++ app/views.py
@@ -1,10 +1,65 @@
 from app import app
 from flask import render_template
+from datetime import datetime
 
+@app.template_filter('clean_date')
+def clean_date(date):
+    return date.strftime("%d %b %Y")
 
 @app.route("/")
 def index():
     return render_template("public/index.html")
+
+@app.route("/jinja")
+def jinja():
+
+    my_name = 'Murilo'
+    age = 31
+    langs = ['Python', 'C#', 'Bash', 'git', ]
+
+    friends = {
+        'Rodrigo': 32,
+        'Luiz': 30,
+        'Dorocinski': 33
+    }
+
+    colours = ('Red', 'Green')
+
+    cool = True
+    class GitRemote:
+        def __init__(self, name, description, url):
+          self.name = name
+          self.description = description
+          self.url = url
+
+        def pull(self):
+            return f"Pulling repo {self.name}"
+
+        def clone(self):
+            return f"Cloning into {self.url}"
+
+    my_remote = GitRemote(
+        name='Flask Jinja',
+        description='Template design tutorial',
+        url='https://github.com/murilobellatini/jinja.git'
+    )
+
+    def repeat(x, qty):
+        return x * qty
+
+    date = datetime.utcnow()
+
+    my_html = "<h1>THIS IS SOME HTML</h1>"
+
+    suspicious = "<script>alert('YOU GOT HACKED!')</script>"
+
+    return render_template(
+        "public/jinja.html", my_name=my_name, age=age, 
+        langs=langs, friends=friends, colours=colours,
+        cool=cool, GitRemote=GitRemote, repeat=repeat,
+        my_remote=my_remote, date=date, my_html=my_html,
+        suspicious=suspicious
+    )
 
 @app.route("/about")
 def about():
