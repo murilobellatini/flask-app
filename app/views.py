16fa7f2c4ab6412300ba950db90039538b27f4f7
Murilo <43999445+murilobellatini@users.noreply.github.com>
2020-07-26 18:15:05 -0300

    Integrate forms

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 from datetime import datetime
 import pandas as pd
 
@@ -67,3 +67,20 @@ def jinja():
 @app.route("/about")
 def about():
     return render_template("public/about.html")
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+
+    if request.method == "POST":
+
+        req = request.form
+
+        username = req["username"]
+        email = req.get("email")
+        password = request.form["password"]
+
+        print(username, email, password)
+
+        return redirect(request.url)
+
+    return render_template("public/sign_up.html")
